8f990071b5434054f46eb3c06b48d9b3147597f1
FinochioAdrian <eidrienhez33@gmail.com>
2024-02-26 12:06:33 -0300

    Moddify name folder

--- Desafio8_Clase19/src/app.js
+++ Desafio8_Clase19/src/app.js
@@ -3,7 +3,12 @@ import __dirname from "./utils.js";
 import connectDB from "./config/db.config.js";
 import handlebars_config from "./config/handlebars.config.js";
 import IOconfig from "./config/IO.config.js";
-
+// service session and login
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import MongoStore from "connect-mongo";
+//envio de mensaje entre vistas
+import flash from 'connect-flash'
 
 //importamos las rutas
 import viewsRouter from "./feature/views/views.router.js";
@@ -14,11 +19,22 @@ const app = express();
 
 connectDB()
 //Iniciamos el Server
-const httpServer =Server(app,8080)
+const PORT = process.env.PORT
+const httpServer =Server(app,PORT)
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(cookieParser())
+app.use(session({
+    store:MongoStore.create({
+        mongoUrl,
+        ttl:300
+    }),
+    secret:"secretCode",
+    resave:true,
+    saveUninitialized:true
+}))
+app.use(flash())
 // Manejo de Handlebars
 handlebars_config(app);
 app.use(express.static(__dirname + "/public"));
