5e8a587676aebcfa57a9f9e1552887ec484dba8c
FinochioAdrian <eidrienhez33@gmail.com>
2024-04-15 23:32:33 -0300

    Add function generateProduct

--- Desafio13_Clase32/src/utils/Mocks.js
+++ Desafio13_Clase32/src/utils/Mocks.js
@@ -7,13 +7,22 @@ export const generateListProducts = await  => {
     for (let i =0 ; i<numOfProducts; i++){
         products.push(generateProduct())
     }
+
+    return products
 }
 export const generateProduct = await  => {
     
    return {
-    
+    _id:faker.database.mongodbObjectId(),
+    title:faker.commerce.productName(),
+    description:faker.commerce.productDescription(),
+    code:faker.string.alphanumeric(5),
+    price:faker.commerce.price(),
+    status:faker.datatype.boolean({probability:0.9}),
+    stock:faker.number.int({ max: 100 }) ,
+    category:faker.commerce.department(),
+    thumbnails:faker.image.url(),
    }
 
 
 }
-
